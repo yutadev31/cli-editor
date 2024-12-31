cf0f0ca4561998c2b5b355ed8822784b1afa52a5
Yuta <jp.yuta0131@gmail.com>
2024-12-31 22:04:28 +0900

    test: utilsのtext.rsのテストを追加

--- crates/utils/src/text.rs
+++ crates/utils/src/text.rs
@@ -1,3 +1,19 @@
 pub fn lines(s: String) -> Vec<String> {
     s.split("\n").map(|s| s.to_string()).collect()
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_lines() {
+        let input = String::from("line1\nline2\nline3");
+        let expected = vec![
+            String::from("line1"),
+            String::from("line2"),
+            String::from("line3"),
+        ];
+        assert_eq!(lines(input), expected);
+    }
+}
