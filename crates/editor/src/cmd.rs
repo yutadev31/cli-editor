671f532b59ddaf54f44ef250465fd7d5950a14c6
Yuta <jp.yuta0131@gmail.com>
2025-01-02 16:22:38 +0900

    update

--- crates/editor/src/cmd.rs
+++ crates/editor/src/cmd.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
 
-use crate::state::EditorState;
+use crate::{e_cursor::EditorCursor, state::EditorState};
 
 pub type Command = Box<dyn Fn(&mut EditorState)>;
 type CommandMap = HashMap<String, Command>;
@@ -26,53 +26,21 @@ impl EditorCommand {
         cmds.register("q", Box::new(|editor| editor.quit()));
         cmds.register("w", Box::new(|editor| editor.write()));
         cmds.register(
-            "wq",
-            Box::new(|editor| {
-                editor.write();
-                editor.quit();
-            }),
-        );
-        cmds.register(
             "x",
             Box::new(|editor| {
                 editor.write();
                 editor.quit();
             }),
         );
+        cmds.register(
+            "wq",
+            Box::new(|editor| {
+                editor.write();
+                editor.quit();
+            }),
+        );
 
-        // Movement
-        cmds.register(
-            "left",
-            Box::new(|editor| editor.cursor.cmd_left(&editor.buf, &mut editor.offset)),
-        );
-        cmds.register(
-            "right",
-            Box::new(|editor| editor.cursor.cmd_right(&editor.buf, &mut editor.offset)),
-        );
-        cmds.register(
-            "up",
-            Box::new(|editor| editor.cursor.cmd_up(&editor.buf, &mut editor.offset)),
-        );
-        cmds.register(
-            "down",
-            Box::new(|editor| editor.cursor.cmd_down(&editor.buf, &mut editor.offset)),
-        );
-        cmds.register(
-            "top",
-            Box::new(|editor| editor.cursor.cmd_top(&editor.buf, &mut editor.offset)),
-        );
-        cmds.register(
-            "bottom",
-            Box::new(|editor| editor.cursor.cmd_bottom(&editor.buf, &mut editor.offset)),
-        );
-        cmds.register(
-            "line_start",
-            Box::new(|editor| editor.cursor.cmd_line_start(&editor.buf)),
-        );
-        cmds.register(
-            "line_end",
-            Box::new(|editor| editor.cursor.cmd_line_end(&editor.buf)),
-        );
+        EditorCursor::register_cmds(&mut cmds);
 
         cmds
     }
